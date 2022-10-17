31781146eb9ab461bd919aa1f6d8130a63b0881e
EMalagoli92 <emala.892@gmail.com>
2022-10-17 22:24:19 +0200

    Update utils.py

--- cvt_tensorflow/models/utils.py
+++ cvt_tensorflow/models/utils.py
@@ -62,21 +62,6 @@ def split_(tensor: tf.Tensor,
                     axis = dim
                     ) 
 
-def _ntuple(n):
-    def parse(x):
-        if isinstance(x, container_abcs.Iterable):
-            return x
-        return tuple(repeat(x, n))
-
-    return parse
-
-
-to_1tuple = _ntuple(1)
-to_2tuple = _ntuple(2)
-to_3tuple = _ntuple(3)
-to_4tuple = _ntuple(4)
-to_ntuple = _ntuple
-
 
 def run_layers_list(input_: tf.Tensor,
                     list_: List[Type[L]]
@@ -99,3 +84,19 @@ def run_layers_list(input_: tf.Tensor,
     for layer in list_:
         x = layer(x)
     return x
+
+
+def _ntuple(n):
+    def parse(x):
+        if isinstance(x, container_abcs.Iterable):
+            return x
+        return tuple(repeat(x, n))
+
+    return parse
+
+
+to_1tuple = _ntuple(1)
+to_2tuple = _ntuple(2)
+to_3tuple = _ntuple(3)
+to_4tuple = _ntuple(4)
+to_ntuple = _ntuple
